597bd9ada6c68d9f9a9f55c4170053488fdb3ccb
zhaoqian1990 <547106430@qq.com>
2020-06-30 21:49:04 +0800

    ## _each optimized

--- build/bundle.js
+++ build/bundle.js
@@ -1,16 +1,15 @@
 const _each = (obj, cb) => {
-  let value,
-      i = 0,
+  let i = 0,
       len = obj.length,
       isArray = Array.isArray(obj);
 
   if (isArray) {
     for (; i < len; i++) {
-      value = cb.call(obj[i], obj[i], i);
+      cb(obj[i], i);
     }
   } else {
     for (i in obj) {
-      value = cb.call(obj[i], obj[i], i);
+      cb(obj[i], i);
     }
   }
 };
